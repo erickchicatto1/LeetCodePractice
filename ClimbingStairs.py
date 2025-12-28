34acbb608c81d771a67edb2c194e24396634247c
erickchicatto1 <51826780+erickchicatto1@users.noreply.github.com>
2025-12-28 06:34:07 -0600

    Add files via upload

---
+++ ClimbingStairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self,n:int)->int:
+        
+        if n==0 or n == 1:
+            return 1
+        
+        prev,curr = 1,1
+        
+        for i in range(2,n+1):
+            temp = curr
+            curr = prev + curr
+            prev = temp
+            
+            
+solution = Solution()
+solution.climbStairs(2)
