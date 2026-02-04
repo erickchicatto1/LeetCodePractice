ebc6294946900261108fd0de30d538badba85340
erickchicatto1 <51826780+erickchicatto1@users.noreply.github.com>
2026-02-04 12:26:19 -0600

    Add files via upload

---
+++ stairCasev2.py
@@ -0,0 +1,12 @@
+def staircase(n):
+    for i in range(0,n):
+        for j in range(0,n):
+            if i + j >= n-1:
+                print("#",end='')
+            else:
+                print(" ",end='')
+        print()
+        
+        
+if __name__ == '__main__':
+    staircase(6)
