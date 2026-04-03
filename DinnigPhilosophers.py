34dc6727f53aac97e10940d394c79ab0e97de684
erickchicatto1 <51826780+erickchicatto1@users.noreply.github.com>
2026-04-03 10:16:08 -0600

    Create DinnigPhilosophers.py

---
+++ DinnigPhilosophers.py
@@ -0,0 +1,26 @@
+class DiningPhilosophers:
+
+    # call the functions directly to execute, for example, eat()
+    def wantsToEat(self,
+                   philosopher: int,
+                   pickLeftFork: 'Callable[[], None]',
+                   pickRightFork: 'Callable[[], None]',
+                   eat: 'Callable[[], None]',
+                   putLeftFork: 'Callable[[], None]',
+                   putRightFork: 'Callable[[], None]') -> None:
+    
+    def pickLeftFork(self):
+        pass
+    
+    def pickRightFork(self):
+        pass
+    
+    def eat(self):
+        pass
+
+    def putLeftFork(self):
+        pass
+    
+    def putRightFork(self):
+        pass
+    
