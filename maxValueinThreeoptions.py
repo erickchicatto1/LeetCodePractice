1a4bb68209cf95e376a42db68b824e1e4bb948d9
erickchicatto1 <51826780+erickchicatto1@users.noreply.github.com>
2026-04-14 09:46:54 -0600

    Create maxValueinThreeoptions.py

---
+++ maxValueinThreeoptions.py
@@ -0,0 +1,13 @@
+a = 120
+b = 95
+c = 300
+
+# TODO: implementar lógica considerando que se dan las variables a, b y c
+if (a>b>c):
+  print(f"a es el maximo valor{a}")
+elif(b>a>c):
+  print(f"b es el maximo valor{b}")
+elif(c>b>a):
+  print(f"c es el maximo valor{c}")
+else:
+  print("otra combinacion")
