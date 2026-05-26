34840ce94e74489a6a166564c0b4e2194460b9cc
ErickChicatto <51826780+erickchicatto1@users.noreply.github.com>
2026-05-26 09:41:10 -0600

    Create AttemptTwoIdentify.py

---
+++ AttemptTwoIdentify.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getLargestOutlier(self, nums: List[int]) -> int:
+        frecuency = {}
+        restante =0
+        totalNumber = 0
+
+        for i in range(len(nums)):
+            totalNumber += nums[i]
+            
+            if nums[i] == :
+                restante = total - nums[i]
+                
+        #To count the frecuency
+        for num in nums:
+            if num in nums:
+                nums[num] += 1     
