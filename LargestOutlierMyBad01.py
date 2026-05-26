46eb63068db56e8d856e5491f1b25f82d005f75a
ErickChicatto <51826780+erickchicatto1@users.noreply.github.com>
2026-05-26 09:12:40 -0600

    Create LargestOutlierMyBad01.py

---
+++ LargestOutlierMyBad01.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getLargestOutlier(self, nums: List[int]) -> int:
+
+        #To calculate the total number
+        totalNumber = 0
+        outlier = 0
+        for i in range(len(nums)):
+            totalNumber += nums[i]
+
+        #sum the number
+        for i in range(len(nums)):
+            if nums[i] + nums[i+1] == nums[i+2]:
+                print("this is the number")
+
+        #obtain the outlier
+        outlier = total - 
+        
+                
+
+        
